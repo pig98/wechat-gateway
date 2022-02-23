f2f2eee8a10b4407cf6de47d15b146f267c78624
pig <pig@net-data.cn>
2022-02-23 23:22:28 +0800

    fix eslint rule

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -3,16 +3,14 @@
 const Controller = require('egg').Controller;
 
 class WeChatController extends Controller {
-  constructor(ctx) {
-    super(ctx)
-  }
   async test() {
     const { ctx } = this;
     ctx.body = 'hi, test';
   }
+
   async token() {
     const { ctx } = this;
-    ctx.helper.success({ctx});
+    ctx.helper.success({ ctx });
   }
 }
 
--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,40 +1,42 @@
-const moment = require('moment')
+'use strict';
+
+const moment = require('moment');
 
 // 格式化时间
-exports.formatTime = time => moment(time).format('YYYY-MM-DD HH:mm:ss')
+exports.formatTime = time => moment(time)
+  .format('YYYY-MM-DD HH:mm:ss');
 
 // 处理成功响应
-exports.success = ({ ctx, res = null, message = '请求成功' })=> {
-    ctx.body = {
-        success: true,
-        message
-    }
-    Object.assign(ctx.body, res)
-    ctx.status = 200
-}
+exports.success = ({ ctx, res = null, message = '请求成功' }) => {
+  ctx.body = {
+    success: true,
+    message,
+  };
+  Object.assign(ctx.body, res);
+  ctx.status = 200;
+};
 
-exports.bad = ({ ctx, message })=> {
-    //res contain code, message
-    ctx.body = {
-        success: false,
-        ...message
-    }
-    ctx.status = 400
-}
+exports.bad = ({ ctx, message }) => {
+  // res contain code, message
+  ctx.body = {
+    success: false,
+    ...message,
+  };
+  ctx.status = 400;
+};
 
-exports.response = ({ ctx, res = null })=> {
-    ctx.status = res.status
-    if(res.status === 200 || res.status === 201) {
-        ctx.body = {
-            data: res.data,
-            success: true
-        }
-    }
-    else {
-        ctx.body = {
-            success: false,
-            error_code: (res.data && res.data.error_code) ? res.data.error_code : (res.data && res.data.error ? res.data.error : res.status),
-            error_message: (res.data && res.data.error_message ? res.data.error_message : (res.data && res.data.error_description ? res.data.error_description : res.data))
-        }
-    }
-}
+exports.response = ({ ctx, res = null }) => {
+  ctx.status = res.status;
+  if (res.status === 200 || res.status === 201) {
+    ctx.body = {
+      data: res.data,
+      success: true,
+    };
+  } else {
+    ctx.body = {
+      success: false,
+      error_code: (res.data && res.data.error_code) ? res.data.error_code : (res.data && res.data.error ? res.data.error : res.status),
+      error_message: (res.data && res.data.error_message ? res.data.error_message : (res.data && res.data.error_description ? res.data.error_description : res.data)),
+    };
+  }
+};
